c4ffd4bc4847a01ffaabc238c3ab23481716b1e6
Cyrill Raccaud <cyrill.raccaud@aposphere.com>
2024-10-10 11:15:48 +0200

    separate smoke test for PRs

---
+++ smoke_test/setup_cookies.py
@@ -0,0 +1,17 @@
+"""Setup cookies for smoke test for cookidoo-api."""
+
+from dotenv import load_dotenv
+
+from cookidoo_api.cookidoo import Cookidoo
+from smoke_test.conftest import save_cookies
+
+load_dotenv()
+
+
+class TestLoginAndValidation:
+    """Test login and validation."""
+
+    async def test_cookidoo_login(self, cookidoo: Cookidoo) -> None:
+        """Test cookidoo validation of the token or login otherwise."""
+        await cookidoo.login()
+        save_cookies(cookidoo.cookies)
