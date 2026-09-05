7bffd15c91b36a3d6a394afd669fad10163409da
Cyrill Raccaud <cyrill.raccaud@aposphere.com>
2026-09-05 10:38:48 +0200

    Format with ruff

--- tests/test_cookidoo.py
+++ tests/test_cookidoo.py
@@ -884,8 +884,10 @@ class TestGetRecipeDetails:
         assert data.step_groups[0].title == ""
         assert len(data.step_groups[0].recipe_steps) == 4
         assert data.step_groups[0].recipe_steps[0].title == "1"
-        assert data.step_groups[0].recipe_steps[0].formatted_text.startswith(
-            "<NOBR>200 g Kokosraspeln</NOBR>"
+        assert (
+            data.step_groups[0]
+            .recipe_steps[0]
+            .formatted_text.startswith("<NOBR>200 g Kokosraspeln</NOBR>")
         )
 
     @pytest.mark.parametrize(
